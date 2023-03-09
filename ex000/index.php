89254370562265c06dd05e91c7e1d5e3d33c5942
Mauro Kruger <122462705+maurokrugerjr@users.noreply.github.com>
2023-03-09 16:37:59 -0300

    arquivos exemplos php

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <h1>
+    <?php 
+        echo "Olá mundo! \u{1F30E}";
+    ?>
+    </h1> 
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sint tempora vitae a repudiandae mollitia aut impedit ipsa ex nostrum, libero amet deleniti veniam debitis facere cupiditate asperiores consequuntur nisi.</p>
+</body>
+</html>
